539d60ff4ca9349f19658d55b00d91933f8345d5
Kyle_Monper <kyle.monper@gmail.com>
2019-01-26 11:08:33 -0800

    removed old practice script

--- 244/CalFire.R
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-library(sf)
-
-fire <- st_read(".", layer = "Fire_Output")
-rx_fire <- st_read(".", layer = "nonfire_rx_Output")
-rx_nonfire <- st_read(".", layer = "nonfire_rx_Output")
