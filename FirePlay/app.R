82c86c76e4cc7be6b3a6e2810e1b2f10766eb681
Kyle_Monper <kyle.monper@gmail.com>
2019-01-27 11:23:34 -0800

    began building slider for selecting data range -- need to figure out how to coerce date (which is a factor) meaningfully into numeric so it can be filtered. OR how to filter factors

--- FirePlay/app.R
+++ FirePlay/app.R
@@ -1,26 +1,36 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+
 
 ui <- fluidPage(
-  leafletOutput("map")
-)
+  leafletOutput("map"),
+  fluidRow(
+    column(4,
+           
+           # Date slider 
+           sliderInput("date_range", label = "Select Date", min = 2000, 
+                       max = 2002, value = c(2000, 2002)))
+    )
+  )
+  
+
 
 server <- function(input, output, session) {
+
+  #code for reacting with the date slider -- need to figure out how to coerce factor(YEAR_) into numeric so that we can filter
+  data_input <- reactive({
+    fire_trans_2007 %>% 
+      filter( YEAR_ >= as.numeric(input$date_range[1])) %>% 
+      filter(YEAR_ <= as.numeric(input$date_range[2]))
+  })
   
+  #this outputs the map
   output$map <- renderLeaflet({
-   
-    leaflet(fire_trans_2007) %>% addTiles() %>%
-     addPolygons()
+    leaflet(fire_trans_2007) %>% 
+      addTiles() %>%
+     addPolygons(
+       fill = data_input()$YEAR_
+     )
   })
   
 }
