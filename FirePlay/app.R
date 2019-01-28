65087882d7904a5a73a26e9eefe375ee30cc9359
Kyle_Monper <kyle.monper@gmail.com>
2019-01-27 18:58:47 -0800

    Changed layout

--- FirePlay/app.R
+++ FirePlay/app.R
@@ -1,10 +1,23 @@
 library(shiny)
+library(tidyverse)
+library(sf)
+library(leaflet)
+library(tmap)
+library(plotly)
 
 
+
+fire_year <- fire %>% 
+  select(YEAR_, FIRE_NAME, Shape_Area) %>% 
+  filter(YEAR_ ==  "2000" | YEAR_ == "2001" | YEAR_ == "2002" | YEAR_ == "2003")
+
+
+#change projection to be compatible with leaflet
+fire_year <- st_transform(fire_year, crs = 4326)
 
 ui <- fluidPage(
   titlePanel("California Fires"),
-  sidebarLayout(
+  sidebarLayout(position = "left",
     mainPanel(leafletOutput("map")),
     sidebarPanel( 
            
@@ -12,14 +25,14 @@ ui <- fluidPage(
            sliderInput("date_range", 
                        label = "Select Date", 
                        min = 2000, 
-                       max = 2002,
-                       value = c(2000, 2002)),
+                       max = 2003,
+                       value = c(2000, 2003)),
            
     # search bar based on fire names
-    tags$div(title = "Search by name of fire", #this creates an information box that displays the text when hovering over the widget
+    tags$div(title = "Search by name of fire", #this creates an information box that displays the text when hovering over the widget 
              selectInput(inputId = "fire", 
                          label = "Fire Name", 
-                         choices = sort(unique(fire_trans_2007$FIRE_NAME))))
+                         choices = sort(unique(fire_year$FIRE_NAME))))
     )
   )
 )
@@ -33,18 +46,19 @@ server <- function(input, output) {
   output$map <- renderLeaflet({
     
     
-    #allows user to highlight a polygon based on the selected date range
-    pal <- colorFactor(
-      palette = "Red",
-      domain = input$date_range
-    )
-    
+      #allows user to highlight a polygon based on the selected date range
+      pal <- colorFactor(
+        palette = "Red",
+        domain = input$date_range
+      )
+      
     #renders the map
     leaflet(fire_trans_2007) %>% 
       addProviderTiles("Esri.WorldTopoMap") %>% 
       addPolygons(
-        fillColor = ~pal(fire_trans_2007$YEAR_)
+        fillColor = ~pal(fire_year$YEAR_)
       )
+   
   })
   
   
