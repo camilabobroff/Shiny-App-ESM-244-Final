cfa5d2d6c2bf0feb43ddd413fadf2b03b5f0bbc4
camilabobroff <camilabobroff@bren.ucsb.edu>
2019-02-22 17:37:32 -0800

    Import new image

--- app.R
+++ app.R
@@ -8,43 +8,50 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  theme = shinytheme("sandstone"),
+  
+  
+  # Application title
+  navbarPage("Playing with Fire... Data",
+             
+             sidebarPanel(
+               tags$img(src='justin_sullivan_getty_images.png', height=200, width=200),
+               tags$figcaption("Justin Sullivan/Getty Images")
+             )
+  ),
+  mainPanel(
+    uiOutput
+  )
+  tabPanel("Map"),
+  tabPanel("Graphs")
+  # Sidebar with a slider input for number of bins 
+  # sidebarLayout(
+  #  sidebarPanel(
+  #  selectInput(inputId = "acres_burned",
+  #  label = "Acres Burned", 
+  #  choices = sort(unique(top100$CAUSE)))),
+  #  mainPanel(
+  #    plotOutput("acresPlot")
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
 
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) { # Added session to see if it would help render the image
+  output$Image <- renderImage({
+    filename <- normalizePath(file.path('./www', 
+                                        paste('image', input$n, '.png', sep='')))
+    
+    list(src = filename)
+    
+  }, deleteFile = FALSE)
+  
+}
 # Run the application 
 shinyApp(ui = ui, server = server)
 
